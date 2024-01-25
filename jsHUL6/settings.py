ec546924397cf813f1753634ae116f5b451a6537
LAPTOP-FMOIC7M3\nv-ma <nv-mail@mail.ru>
2024-01-25 23:20:50 +0300

    added scroll posts app

--- jsHUL6/settings.py
+++ jsHUL6/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'scroll',
     'singlepage',
     'django.contrib.admin',
     'django.contrib.auth',
--- jsHUL6/urls.py
+++ jsHUL6/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('scroll/', include('scroll.urls')),
     path('', include('singlepage.urls')),
 ]
