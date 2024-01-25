8833b8dec74b477e28b77e78c739bb7ca8dbf108
LAPTOP-FMOIC7M3\nv-ma <nv-mail@mail.ru>
2024-01-25 20:26:26 +0300

    last changes to singlepage project

--- singlepage/urls.py
+++ singlepage/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'singlepage'
+# app_name = 'singlepage'
 
 urlpatterns = [
-    # path('', views.index, name = 'index'),
+    path('', views.index, name = 'index'),
+    path('sections/<int:num>', views.section, name = 'section'),
     path('<str:name>', views.hello, name = 'hello'),
 ]
--- singlepage/views.py
+++ singlepage/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 # Create your views here.
+def index(request):
+	return render(request, 'singlepage/index.html')
+
+texts = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum.',
+ 'Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+ 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp qui officia deserunt mollit anim id est laborum.']
+
+# i = 0
+# for s in texts:
+# 	print(i, ' : ', s)
+# 	i += 1
+
+# for i in range(len(texts)):
+# 	print(i, ' : ', texts[i])
+
+def section(request, num):
+	if 1 <= num <= 3:
+		return HttpResponse(texts[num-1])
+	else:
+		raise Http404('No such section')
+		# return HttpResponse('No such section')
+
 def hello(request, name):
 	return HttpResponse(f' glad to see you, {name.capitalize()}, and hello from the JS_Singlepage_app! ')
